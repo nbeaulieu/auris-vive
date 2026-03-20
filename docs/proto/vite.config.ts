72557d6d5fd17608207c3a6057897eddd1783acd
nbeaulieu <nbeaulieu@me.com>
2026-03-19 22:20:14 -0700

    Updated pages settings.

--- docs/proto/vite.config.ts
+++ docs/proto/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  base: '/auris-vive/proto/',
+  base: '/auris-vive/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
   },
